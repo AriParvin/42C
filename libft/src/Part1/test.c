69bf43ed1f20f3e951c8eb5acd43635e321915f9
Arian Parvin <aparvin@1b1c2.42berlin.de>
2022-12-12 14:55:16 +0100

    color shenanigans

--- libft/src/Part1/test.c
+++ libft/src/Part1/test.c
@@ -6,11 +6,18 @@
 /*   By: aparvin <aparvin@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 12:40:07 by aparvin           #+#    #+#             */
-/*   Updated: 2022/12/02 17:17:08 by aparvin          ###   ########.fr       */
+/*   Updated: 2022/12/12 14:54:40 by aparvin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 #include <string.h>
+#define	RED "\x1b[31m"
+#define	GRN "\x1b[32m"
+#define	YLO "\x1b[33m"
+#define	BLU "\x1b[34m"
+#define	MAG "\x1b[35m"
+#define	CYN "\x1b[36m"
+#define	RST "\x1b[0m"
 
 #include "ft_isalpha.c"
 #include "ft_isdigit.c"
@@ -38,25 +45,30 @@
 #include "ft_strdup.c"
 */
 
-int main(void)
+int main()
 {	
 	int t1 = 'a';
-	char t2[7] = "FooBar";
+	char *str1 = "FooBar";
 	size_t n = 1;
 
-	printf("@\t@@@@@\t@@@@\t\t@@@@@\t@@@@@\n");
+	printf(YLO"@\t@@@@@\t@@@@\t\t@@@@@\t@@@@@\n");
 	printf("@\t  @  \t@   @\t\t@\t  @  \n");
 	printf("@\t  @  \t@@@@ \t\t@@@@\t  @  \n");
 	printf("@\t  @  \t@   @\t\t@  \t  @  \n");
-	printf("@@@@@\t@@@@@\t@@@@@\t@@@@@\t@  \t  @  \n\n");
+	printf("@@@@@\t@@@@@\t@@@@@\t@@@@@\t@  \t  @  \n\n"RST);
 
-	printf("isalpha(%c)\nSL:\t%d\n",t1, isalpha(t1));
-	printf("42:\t%d\n\n", ft_isalpha(t1));
+	printf("PART 1\n\n");
 
-	printf("isdigit(%c)\nSL:\t%d\n",t1, isdigit(t1));
+	printf(CYN"isalpha(%c)\n"RST, t1);
+	printf(GRN"SL:\t%d\n", isalpha(t1));
+	printf("42:\t%d\n\n"RST, ft_isalpha(t1));
+
+	printf("isdigit(%c)\n", t1);
+	printf("SL:\t%d\n", isdigit(t1));
 	printf("42:\t%d\n\n", ft_isdigit(t1));
 
-	printf("isalnum(%c)\nSL:\t%d\n",t1, isalnum(t1));
+	printf("isalnum(%c)\n", t1);
+	printf("SL:\t%d\n", isalnum(t1));
 	printf("42:\t%d\n\n", ft_isalnum(t1));
 	
 	printf("isascii(%c)\nSL:\t%d\n",t1, isascii(t1));
@@ -65,11 +77,11 @@ int main(void)
 	printf("isprint(%c)\nSL:\t%d\n",t1, isprint(t1));
 	printf("42:\t%d\n\n", ft_isprint(t1));
 
-	printf("strlen(%s)\nSL:\t%ld\n",t2, strlen(t2));
-	printf("42:\t%ld\n\n", ft_strlen(t2));
+	printf("strlen(%s)\nSL:\t%ld\n",str1, strlen(str1));
+	printf("42:\t%ld\n\n", ft_strlen(str1));
 
-	printf("memset(%s)\nSL:\t%p\n","FooBar",memset(t2,t1,1));
-	printf("42:\t%p\n\n", memset(t2,t1,1));
+	printf("memset(%s)\nSL:\t%p\n","FooBar",memset(str1,t1,1));
+	printf("42:\t%p\n\n", memset(str1,t1,1));
 
 	char bz1[] = "FooBar";
 	char bz2[] = "FooBar";

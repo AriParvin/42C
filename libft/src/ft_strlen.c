59631c53ae48bf1ef92dec15b96e912730c0cea8
AriParvin <arian.parvin@parvin.de>
2022-12-16 08:30:57 +0100

    state 1610

--- libft/src/ft_strlen.c
+++ libft/src/ft_strlen.c
@@ -5,16 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aparvin <aparvin@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/12 13:26:47 by aparvin           #+#    #+#             */
-/*   Updated: 2022/12/12 13:28:09 by aparvin          ###   ########.fr       */
+/*   Created: 2022/11/28 12:47:35 by aparvin           #+#    #+#             */
+/*   Updated: 2022/12/09 10:15:17 by aparvin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <string.h>
+#include <stdio.h>
 
-size_t	ft_strlen(const char *str)
+size_t	ft_strlen(char *s)
 {
-	size_t len;
+	char	*p;
 
-	while (*str++)
-		len++;
-	return (len);
+	p = s;
+	while (*p)
+		p++;
+	return (p - s);
 }
+/*
+int main()
+{
+	char* str = "Hello World";
+	printf("SL:\t%ld\n", strlen(str));
+	printf("42:\t%ld\n", ft_strlen(str));
+}
+*/

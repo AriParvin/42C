652f267c86fe86425774c43e0e2eb2dcc06dcd7f
Arian Parvin <aparvin@1b1c2.42berlin.de>
2023-04-06 12:17:39 +0200

    push_swap solution found

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -5,75 +5,94 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aparvin <aparvin@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/04 15:13:01 by aparvin           #+#    #+#             */
-/*   Updated: 2023/04/04 15:13:33 by aparvin          ###   ########.fr       */
+/*   Created: 2023/04/06 11:59:40 by aparvin           #+#    #+#             */
+/*   Updated: 2023/04/06 11:59:46 by aparvin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-# include "libft.h"
 
-typedef struct		s_stack
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+# include <stdio.h>
+
+typedef struct s_stack
 {
-	int				num;
-	int				diff;
+	int				value;
+	int				index;
+	int				pos;
+	int				target_pos;
+	int				cost_a;
+	int				cost_b;
 	struct s_stack	*next;
-}					t_stack;
+}	t_stack;
 
-typedef struct		s_com
-{
-	int				rotator;
-	char			*final;
-}					t_com;
+/* Initialization */
 
-t_stack				*create_new_node(void);
-t_stack				*copy_args_in_stack(int argc, char **argv);
-void				ft_swap(int *a, int *b);
-int					partition(int *stack1, int start, int end);
-void				quick_sort(int *stack1, int start, int end);
-void				print_final_result(char *origin);
-int					sab(t_stack **head);
-int					pab(t_stack **head_to, t_stack **head_from);
-int					rab(t_stack **head);
-int					rrab(t_stack **head);
-int					ss(t_stack **stack1, t_stack **stack2);
-int					rr(t_stack **stack1, t_stack **stack2);
-int					rrr(t_stack **stack1, t_stack **stack2);
-int					s_len(t_stack *stack);
-int					*create_array_from_list(t_stack *stack, int len);
-int					find_mid_value(t_stack *stack, int len);
-t_com				*new_result(void);
-char				*add_to_string(char *to, char *add);
-int					div_stack_a(t_stack **stack1, t_stack **stack2,
-	t_com **result, int len);
-int					div_stack_b(t_stack **stack1, t_stack **stack2,
-	t_com **result, int len);
-void				comb_1(t_stack **stack, t_com **result);
-void				comb_2(t_stack **stack, t_com **result);
-void				comb_3(t_stack **stack, t_com **result);
-void				comb_4(t_stack **stack, t_com **result);
-void				comb_5(t_stack **stack, t_com **result);
-void				comb_6(t_stack **stack, t_com **result);
-void				comb_7(t_stack **stack, t_com **result);
-void				comb_8(t_stack **stack, t_com **result);
-void				comb_9(t_stack **stack, t_com **result);
-void				swap_three_a(t_stack **stack, t_com **result);
-void				swap_three_b(t_stack **stack, t_com **result);
-void				swap_three(t_stack **stack, t_com **result);
-void				swap_two(t_stack **stack, t_com **result);
-void				push_back(t_stack **stack1, t_stack **stack2,
-	t_com **result, int half);
-void				rotate_back(t_stack **stack, t_com **result, int back);
-void				swap_elements(t_stack **stack1, t_stack **stack2,
-	t_com **result, int len);
-void				stack_del(t_stack **stack);
-void				doubles_checker(t_stack **stack);
-int					check_int_overflow(char *str);
-int					check_argv(char *str);
-int					add_pa_pb(char **origin, char **res, int i);
-int					add_ra_rra(char **origin, char **res, int i);
-int					add_rb_rrb(char **origin, char **res, int i);
-int					add_others(char **tmp, char **res, int i);
+t_stack		*fill_stack_values(int ac, char **av);
+void		assign_index(t_stack *stack_a, int ac);
+
+/* Sorting Algorithms */
+
+int			is_sorted(t_stack *stack);
+void		tiny_sort(t_stack **stack);
+void		sort(t_stack **stack_a, t_stack **stack_b);
+
+/* Position */
+
+int			get_lowest_index_position(t_stack **stack);
+void		get_target_position(t_stack **stack_a, t_stack **stack_b);
+
+/* Cost */
+
+void		get_cost(t_stack **stack_a, t_stack **stack_b);
+void		do_cheapest_move(t_stack **stack_a, t_stack **stack_b);
+
+/* Calculate Move */
+
+void		do_move(t_stack **a, t_stack **b, int cost_a, int cost_b);
+
+/* Operations */
+
+void		do_pa(t_stack **stack_a, t_stack **stack_b);
+void		do_pb(t_stack **stack_a, t_stack **stack_b);
+void		do_sa(t_stack **stack_a);
+void		do_sb(t_stack **stack_b);
+void		do_ss(t_stack **stack_a, t_stack **stack_b);
+void		do_ra(t_stack **stack_a);
+void		do_rb(t_stack **stack_b);
+void		do_rr(t_stack **stack_a, t_stack **stack_b);
+void		do_rrr(t_stack **stack_a, t_stack **stack_b);
+void		do_rra(t_stack **stack_a);
+void		do_rrb(t_stack **stack_b);
+
+/* Stack Functions */
+
+t_stack		*get_stack_bottom(t_stack *stack);
+t_stack		*get_stack_before_bottom(t_stack *stack);
+t_stack		*stack_new(int value);
+void		stack_add_bottom(t_stack **stack, t_stack *new);
+int			get_stack_size(t_stack	*stack);
+
+/* Utils */
+
+void		free_stack(t_stack **stack);
+long int	ft_atoi(const char *str);
+void		ft_putstr(char *str);
+int			nb_abs(int nb);
+
+/* Error */
+
+void		exit_error(t_stack **stack_a, t_stack **stack_b);
+
+/* Input Check */
+
+int			is_correct_input(char **av);
+int			is_digit(char c);
+int			is_sign(char c);
+int			nbstr_cmp(const char *s1, const char *s2);
 
 #endif

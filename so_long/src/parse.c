749100b650912ebcade78e2d012c10139e991a4c
Arian Parvin <aparvin@1b1c2.42berlin.de>
2023-07-05 16:57:13 +0200

    map render implemented

--- so_long/src/parse.c
+++ so_long/src/parse.c
@@ -29,3 +29,28 @@ size_t      count_bugs(t_game *game)
         }
     }
 }
+
+char        *read_map(char *map)
+{
+    char    *line;
+    char    *line_joined;
+    int     fd;
+
+    fd = open(map, O_RDONLY);
+    line_joined = ft_calloc(1, 1);
+    if (!line_joined)
+        return (NULL);
+    while (1)
+    {
+        line = get_next_line(fd);
+        if (line)
+        {
+            line_joined = ft_strjoin(line_joined, line);
+            free(line);
+        }
+        else
+            break ;
+    }
+    close (fd);
+    return (line_joined);
+}
---
+++ so_long/src/render.c
@@ -0,0 +1,64 @@
+#include "so_long.h"
+
+void    image_select(t_game *data, size_t y, size_t x)
+{
+    size_t  img_size;
+
+    img_size = 32;
+    if (data->grid[y][x] == '1')
+        if (mlx_image_to_window(data->mlx, data->img->wall,
+                x * PIXELS, y * PIXELS) < 0)
+            error_message("MAP RENDER FAILURE");
+    if (data->grid[y][x] == 'C')
+        if (mlx_image_to_window(data->mlx, data->img->bug,
+                x * PIXELS + img_size / 2, y * PIXELS + img_size / 2) < 0)
+            error_message("MAP RENDER FAILURE");
+    if (data->grid[y][x] == 'P')
+        if (mlx_image_to_window(data->mlx, data->img->frog,
+                x * PIXELS, y * PIXELS) < 0)
+            error_message("MAP RENDER FAILURE");
+    if (data->grid[y][x] == 'E')
+        if (mlx_image_to_window(data->mlx, data->img->exit_off,
+                x * PIXELS, y * PIXELS) < 0)
+            error_message("MAP RENDER FAILURE");
+}
+
+void    fill_background(t_game *data)
+{
+    size_t  x;
+    size_t  y;
+
+    x = 0;
+    y = 0;
+    while (y < data->height)
+    {
+        x = 0;
+        while (x < data->width)
+        {
+            if (mlx_image_to_window(data->mlx, data->img->floor,
+                    x * PIXELS, y * PIXELS) < 0)
+            error_message("MAP RENDER FAILURE");
+            x++;
+        }
+        y++;
+    }
+}
+
+void    render_map(t_game *data)
+{
+    size_t  x;
+    size_t  y;
+
+    x = 0;
+    y = 0;
+    while (y < data->height)
+    {
+        x = 0;
+        while (x < data->width)
+        {
+            image_select(data, y, x)
+            x++;
+        }
+        y++;
+    }
+}
